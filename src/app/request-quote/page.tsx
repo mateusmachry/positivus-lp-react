275d13a802177f3fc517d277fe64f55cc0c18736
Mateus <mateusmachry2016@gmail.com>
2025-05-28 15:27:18 -0300

    added script to request-quote page

--- src/app/request-quote/page.tsx
+++ src/app/request-quote/page.tsx
@@ -1,7 +1,15 @@
+import Script from "next/script";
+
 export default function RequestQuotePage() {
     return (
-        <div className="flex flex-col justify-center items-center min-h-screen w-full">
-            <span>REQUEST A QUOTE PAGE</span>
-        </div>
+        <>
+            <div className="flex flex-col justify-center items-center min-h-screen w-full">
+                <span>REQUEST A QUOTE PAGE</span>
+            </div>
+            <Script
+                src="https://www.usevariantly.com/variantly.js"
+                strategy="beforeInteractive"
+            />
+        </>
     )
 }
