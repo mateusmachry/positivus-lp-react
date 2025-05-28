41ee97606acb27fa33d3e9d61dac5d794d38943d
Mateus <mateusmachry2016@gmail.com>
2025-05-27 23:51:59 -0300

    updated script url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         <Footer />
       </div>
       <Script
-        src="https://usevariantly.com/variantly.js"
+        src="https://www.usevariantly.com/variantly.js"
         strategy="beforeInteractive"
       />
     </>
